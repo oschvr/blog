1f5c70f2f0a0b936acc0affd794b7adb94486068
oschvr <oscar@leantech.me>
2020-08-31 23:04:21 +0100

    Remove problems folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,12 +43,5 @@ module.exports = {
         path: `${__dirname}/src/posts/`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/src/problems/`,
-      },
-    },
   ],
 }
