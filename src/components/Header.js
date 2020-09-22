1ceb5813f4610c315b62cd1679d7a13c8041d1a1
oschvr <oscar@leantech.me>
2020-09-22 23:01:26 +0100

    Add uses.mdx

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled'
+
+const Header = styled.div`
+  max-width: 825px;
+  margin: 10px auto;
+  padding: 20px;
+  width: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  h3 {
+    margin: 0;
+    color: #0a0a0a;
+    width: 100%;
+    font-size: 0.85rem;
+  }
+  .navLink {
+    padding-right: 25px;
+  }
+  .nav {
+    padding-top: 10px;
+  }
+  a {
+    font-size: 0.75rem;
+  }
+`
+
+export default Header
