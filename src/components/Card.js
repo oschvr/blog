84e8d2be2524ac74eeec5b035a05102aa5ba277d
oschvr <oscar@leantech.me>
2020-09-24 08:20:28 +0100

    style

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,12 +5,17 @@ const Card = styled.div`
   background: #efeeee;
   box-shadow: 10px 10px 20px #c9c8c8, -10px -10px 20px #ffffff;
 
+  border: 1px solid rgba(255, 255, 255, 0.2);
   margin: 45px auto;
   text-align: left;
   max-width: 800px;
   width: auto;
   padding: 60px ${props => props.padding || 95}px;
 
+  transition: background-color 0.5s ease;
+  &:hover {
+    background: linear-gradient(145deg, #d7d6d6, #ffffff);
+  }
   @media only screen and (max-width: 1024px) {
     padding: 50px 20px;
     margin: 25px;
