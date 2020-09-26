3c210cc0a2c7b5ada8d2e04c26cb1965c952086d
oschvr <oscar@leantech.me>
2020-09-26 21:51:49 +0100

    Update link style

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,16 @@
 import styled from '@emotion/styled'
 
 const Card = styled.div`
-  background: #fff;
-  margin: 20px auto;
+  border-radius: 10px;
+  background: #efeeee;
+  box-shadow: 10px 10px 20px #c9c8c8, -10px -10px 20px #ffffff;
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  margin: 45px auto;
   text-align: left;
-  border: 1px solid #d6d6d6;
-  box-shadow: 3px 3px 0px #bababa;
-  border-radius: 3px;
   max-width: 800px;
   width: auto;
-  padding: 50px ${props => props.padding || 85}px;
-  transition: box-shadow 0.3s ease-in-out;
-
-  &:hover {
-    box-shadow: 6px 6px 0px #bababa;
-  }
-
-  @media only screen and (max-width: 420px) {
-    padding: 50px 20px;
-    margin: 0 5px 10px 5px;
-  }
+  padding: 60px ${props => props.padding || 95}px;
+  transition: background-color 0.5s ease;
 `
 
 export default Card
